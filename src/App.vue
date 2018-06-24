50a5c68bf2ba18ec370f11931236a241973c79d4
M. ALZEN <mszen579@hotmail.com>
2018-06-24 20:28:54 +0100

    create vue project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/MSZ.png" width="100px">
+    <HelloWorld msg="Hello World... Welcome to VUE"/>
+    <h1>Hi, My name is Mohammad, and this is my first VUE app</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
